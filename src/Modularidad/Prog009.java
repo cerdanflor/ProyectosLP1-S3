6d7be55aba4f416b0b98213c6a4fb6224f8d88d2
Flor Cerdán León <cerdanflor@gmail.com>
2020-07-07 12:21:28 -0500

    Prog008 Mét. que reciben parámetros y Prog009 mét. que devuelven valores

---
+++ src/Modularidad/Prog009.java
@@ -0,0 +1,13 @@
+package Modularidad;
+// Flor Cerdán
+public class Prog009 {
+    // Métodos que devuelven valores..
+    public static String miuniversidad(){
+        String universidad = "Universidad Nacional Tecnológica de Lima Sur";
+        return universidad;
+    }
+    public static void main(String[] args) {
+        System.out.println(miuniversidad());
+        System.out.println(miuniversidad().toUpperCase());
+    }
+}
